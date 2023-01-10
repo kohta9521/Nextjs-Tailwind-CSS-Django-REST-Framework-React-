59b5ae44f5cd7f0b248750b4e4e3f923800e7134
Kohta9521 <kohta0421kochi@gmail.com>
2023-01-11 06:54:30 +0900

    add layout.js about header

--- project1/components/Layout.js
+++ project1/components/Layout.js
@@ -9,7 +9,13 @@ export default function Layout({ children, title = "HP by Nextjs" }) {
             </Head>
             <header>
                 <nav className="bg-gray-800 w-screen">
-
+                    <div className="h-14  pl-8 items-center flex">
+                        <div className="flex space-x-4">
+                            <Link href="/" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Home</Link>
+                            <Link href="/blog-page" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Blog</Link>
+                            <Link href="/contact-page" className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">Contact</Link>
+                        </div>
+                    </div>
                 </nav>
             </header>
             <main className="flex flex-1 justify-center items-center flex-col w-screen">
